3dab331b1276579cbf2f95d084798d7bdc8e2900
DanielV <daniel@vikstrom.name>
2024-01-18 09:58:10 +0000

    Fix remaining properties

--- sensor.py
+++ sensor.py
@@ -55,74 +55,37 @@ class MeasurementSensor(CoordinatorEntity, SensorEntity):
         super().__init__(coordinator)
         self._account = account
         self._latest_measurement = meas
+        
         self._attr_unique_id = "%s_account%s_%s" % (
             self.coordinator.data.id,
             self._account.id,
             self._latest_measurement.parameter.replace(" ", "_").replace("-", "_").lower(),
         )
-
-    @callback
-    def _handle_coordinator_update(self) -> None:
-        """Handle updated data from the coordinator."""
-        try:
-            self._latest_measurement = self.coordinator.data.get_measurement(
-                self._account.id, self._latest_measurement.parameter
-            )
-            self.async_write_ha_state()
-        except StopIteration:
-            _LOGGER.error(
-                "Could not find a measurement matching id:%s and parameter:%s",
-                self._account.id,
-                self._latest_measurement.parameter,
-            )
-
-    @property
-    def _attr_name(self) -> str:
-        return "%s %s" % (self._account.full_name, self._latest_measurement.parameter)
-
-    @property
-    def _attr_device_info(self) -> DeviceInfo:
-        """Return a inique set of attributes for each vehicle."""
-        return DeviceInfo(
+        self._attr_name = "%s %s" % (
+            self._account.full_name,
+            self._latest_measurement.parameter
+        )
+        self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, self._account.id)},
             name=self._account.full_name,
             model="%sm3" % self._account.volume,
             manufacturer="PoolLab",
         )
-
-    @property
-    def _attr_native_value(self):
-        return self._latest_measurement.value
-
-    @property
-    def _attr_native_unit_of_measurement(self) -> str:
-        return self._latest_measurement.unit.split(" ")[0]
-
-    @property
-    def _attr_suggested_display_precision(self) -> int:
+        self._attr_native_unit_of_measurement = self._latest_measurement.unit.split(" ")[0]
         try:
             meas_value = float(self._latest_measurement.value)
             if meas_value >= 100:
-                return 0
+                self._attr_suggested_display_precision = 0
             elif meas_value >= 10:
-                return 1
+                self._attr_suggested_display_precision = 1
             else:
-                return 2
+                self._attr_suggested_display_precision = 2
         except:
-            return 1
-
-    @property
-    def _attr_state_class(self) -> SensorStateClass:
-        return SensorStateClass.MEASUREMENT
-
-    @property
-    def _attr_icon(self) -> str:
-        return "mdi:water-percent"
-
-    @property
-    def _attr_extra_state_attributes(self):
-        """Provide attributes for the entity"""
-        return {
+            pass
+        self._attr_state_class = SensorStateClass.MEASUREMENT
+        self._attr_icon = "mdi:water-percent"
+        self._attr_native_value = self._latest_measurement.value
+        self._attr_extra_state_attributes = {
             "measured_at": self._latest_measurement.timestamp,
             "measure": self._latest_measurement.id,
             "ideal_low": self._latest_measurement.ideal_low,
@@ -131,3 +94,29 @@ class MeasurementSensor(CoordinatorEntity, SensorEntity):
             "operator_name": self._latest_measurement.operator_name,
             "comment": self._latest_measurement.comment,
         }
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        try:
+            self._latest_measurement = self.coordinator.data.get_measurement(
+                self._account.id, self._latest_measurement.parameter
+            )
+            self._attr_native_value = self._latest_measurement.value
+            self._attr_extra_state_attributes = {
+                "measured_at": self._latest_measurement.timestamp,
+                "measure": self._latest_measurement.id,
+                "ideal_low": self._latest_measurement.ideal_low,
+                "ideal_high": self._latest_measurement.ideal_high,
+                "device_serial": self._latest_measurement.device_serial,
+                "operator_name": self._latest_measurement.operator_name,
+                "comment": self._latest_measurement.comment,
+            }
+            self.async_write_ha_state()
+        except StopIteration:
+            _LOGGER.error(
+                "Could not find a measurement matching id:%s and parameter:%s",
+                self._account.id,
+                self._latest_measurement.parameter,
+            )
+
